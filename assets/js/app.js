edf2ae9d0d59bc0a3c283fbb66c5786683fecee2
Florian Moser <products@famoser.ch>
2018-02-23 23:03:25 +0100

    removing javascript garbage

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,25 +2,6 @@ require("../sass/app.sass");
 var $ = require("jquery");
 var bootstrap = require("bootstrap");
 
-//open navigation menu
-var openMenu = function () {
-    var menu = $(".menu-content");
-    menu.toggleClass("open");
-    $(".menu-toggle").toggleClass("open");
-    $(".nav-icon").toggleClass("open");
-
-    //close help
-    if ($(".help-content").hasClass("open") && menu.hasClass("open")) {
-        openHelp();
-    }
-};
-
-//open help menu
-var openHelp = function () {
-    $(".help-content").toggleClass("open");
-    $(".help-toggle").toggleClass("open");
-};
-
 //prevent double submit & give user instant feedback
 var disableFormButton = function () {
     var $form = $(this);
@@ -31,7 +12,5 @@ var disableFormButton = function () {
 };
 
 $(document).ready(function () {
-    $("#menu-toggle").on("click", openMenu);
-    $("#help-toggle").on("click", openHelp);
     $("form").on("submit", disableFormButton);
 });
